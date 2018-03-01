4a8a87b5a47e1138573260d62fc0f8a7624b7e44
dantleech <dan.t.leech@gmail.com>
2018-03-01 22:25:10 +0100

    Refactoring

--- lib/Report/Generator/Table/CompareGenerator.php
+++ lib/Report/Generator/Table/CompareGenerator.php
@@ -35,66 +35,8 @@ class CompareGenerator
         $compareFields = $config['compare_fields'];
 
         return F\map($tables, function ($table) use ($conditions, $compare, $compareFields) {
-            $groups = F\group($table, function ($row) use ($conditions) {
-                $values = array_intersect_key($row->getArrayCopy(), array_flip($conditions));
-
-                return F\reduce_left($values, function ($value, $i, $c, $reduction) {
-                    return $reduction . $value;
-                });
-            });
-
-            $table = [];
-            $colNames = null;
-            foreach ($groups as $group) {
-                $firstRow = null;
-                foreach ($group as $row) {
-                    if (null === $firstRow) {
-                        $firstRow = $row->newInstance(array_diff_key($row->getArrayCopy(), array_flip($this->statKeys)));
-                        if (isset($firstRow[$compare])) {
-                            unset($firstRow[$compare]);
-                        }
-                        foreach ($compareFields as $compareField) {
-                            if (isset($firstRow[$compareField])) {
-                                unset($firstRow[$compareField]);
-                            }
-                        }
-                    }
-
-                    if (null === $colNames) {
-                        $colNames = array_combine($firstRow->getNames(), $firstRow->getNames());
-                    }
-
-                    $compared = $row[$compare];
-
-                    foreach ($compareFields as $compareField) {
-                        $name = $compare . ':' . $compared . ':' . $compareField;
-
-                        $name = $this->resolveCompareColumnName($firstRow, $name);
-
-                        $firstRow[$name] = $row[$compareField];
-                        $colNames[$name] = $name;
-
-                        // TODO: This probably means the field is non-comparable, could handle this earlier..
-                        if (isset($this->classMap[$compareField])) {
-                            // we invent a new col name here, use the compare field's class.
-                            $this->classMap[$name] = $this->classMap[$compareField];
-                        }
-                    }
-                }
-
-                $table[] = $firstRow;
-            }
-
-            $table = F\map($table, function ($row) use ($colNames) {
-                $newRow = $row->newInstance([]);
-                foreach ($colNames as $colName) {
-                    $newRow[$colName] = isset($row[$colName]) ? $row[$colName] : null;
-                }
-
-                return $newRow;
-            });
-
-            return $table;
+            $groups = $this->groupByCols($table, $conditions);
+            return $this->processTable($table, $groups, $compare, $compareFields);
         });
 
         return $tables;
@@ -122,5 +64,73 @@ class CompareGenerator
         }
 
         return $this->resolveCompareColumnName($row, $name, $index);
+    }
+
+    private function processTable(array $table, array $groups, string $compare, array $compareFields)
+    {
+        $table = [];
+        $colNames = null;
+        foreach ($groups as $group) {
+            $firstRow = null;
+            foreach ($group as $row) {
+                if (null === $firstRow) {
+                    $firstRow = $row->newInstance(array_diff_key($row->getArrayCopy(), array_flip($this->statKeys)));
+                    if (isset($firstRow[$compare])) {
+                        unset($firstRow[$compare]);
+                    }
+                    foreach ($compareFields as $compareField) {
+                        if (isset($firstRow[$compareField])) {
+                            unset($firstRow[$compareField]);
+                        }
+                    }
+                }
+        
+                if (null === $colNames) {
+                    $colNames = array_combine($firstRow->getNames(), $firstRow->getNames());
+                }
+        
+                $compared = $row[$compare];
+        
+                foreach ($compareFields as $compareField) {
+                    $name = $compare . ':' . $compared . ':' . $compareField;
+        
+                    $name = $this->resolveCompareColumnName($firstRow, $name);
+        
+                    $firstRow[$name] = $row[$compareField];
+                    $colNames[$name] = $name;
+        
+                    // TODO: This probably means the field is non-comparable, could handle this earlier..
+                    if (isset($this->classMap[$compareField])) {
+                        // we invent a new col name here, use the compare field's class.
+                        $this->classMap[$name] = $this->classMap[$compareField];
+                    }
+                }
+            }
+        
+            $table[] = $firstRow;
+        }
+        
+        $table = F\map($table, function ($row) use ($colNames) {
+            $newRow = $row->newInstance([]);
+            foreach ($colNames as $colName) {
+                $newRow[$colName] = isset($row[$colName]) ? $row[$colName] : null;
+            }
+        
+            return $newRow;
+        });
+        
+        return $table;
+    }
+
+    private function groupByCols(array $table, array $conditions)
+    {
+        $groups = F\group($table, function ($row) use ($conditions) {
+            $values = array_intersect_key($row->getArrayCopy(), array_flip($conditions));
+        
+            return F\reduce_left($values, function ($value, $i, $c, $reduction) {
+                return $reduction . $value;
+            });
+        });
+        return $groups;
     }
 }
